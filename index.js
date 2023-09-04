958f3434855f1a2bce175a01011c59dc35c30af6
shubhambargal2002 <shubhambargal2002@gmail.com>
2023-09-04 23:49:54 +0530

    created simple server/backend

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const cors = require("cors");
+
+// middlewares
+app.use(express.json());
+app.use(cors());
 
 app.listen(port,()=>{
     console.log(`I-notebook server is listening at https://localhost:${port}`)
