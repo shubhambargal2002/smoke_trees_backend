5624197cb7ce25bdf3e7149e20db7661f227ba22
shubhambargal2002 <shubhambargal2002@gmail.com>
2023-09-06 00:21:08 +0530

    added process.env.port in port

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const port = 5000;
 const cors = require("cors");
 require("dotenv").config()
+const port=process.env.PORT || 5000;
 
 // middlewares
 app.use(express.json());
